cc58a77b256057df2e84ce6e5ed948554feae8c1
Gerardo Miranda <gerardodev25@gmail.com>
2022-12-29 22:59:43 -0400

    011 Crear Pokémon en base de datos

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Pokemon } from './entities/pokemon.entity';
 
 @Injectable()
 export class PokemonService {
+  constructor(
+    @InjectModel(Pokemon.name) private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
   findAll() {
     return `This action returns all pokemon`;
   }
@@ -12,9 +19,12 @@ export class PokemonService {
     return `This action returns a #${id} pokemon`;
   }
 
-  create(data: CreatePokemonDto) {
+  async create(data: CreatePokemonDto) {
     data.name = data.name.toLowerCase();
-    return data;
+
+    const pokemon = await this.pokemonModel.create(data);
+
+    return pokemon;
   }
 
   update(id: number, data: UpdatePokemonDto) {
