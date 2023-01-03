65a1e8c23d2af7f28d50c8b1ab8719893a98aaf4
Gerardo Miranda <gerardodev25@gmail.com>
2023-01-03 10:20:29 -0400

    008 Resolución - Insertar Pokemons por lote

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,22 +1,30 @@
+import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import axios, { AxiosInstance } from 'axios';
+
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
 
+  constructor(
+    @InjectModel(Pokemon.name) private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
   async executeSeed() {
     const { data } = await this.axios.get<PokeResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=10',
     );
 
-    data.results.forEach(({ name, url }) => {
+    data.results.forEach(async ({ name, url }) => {
       const secments = url.split('/');
       const no = +secments[secments.length - 2];
-      console.log({ name, no });
+      await this.pokemonModel.create({ name, no });
     });
 
-    return data.results;
+    return 'data.results';
   }
 }
