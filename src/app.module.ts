8c9b8b3342a079697372dae5ff524e41738fb16b
Gerardo Miranda <gerardodev25@gmail.com>
2023-01-04 09:46:44 -0400

    005 Configuration Loader

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,22 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
+
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
-import { ConfigModule } from '@nestjs/config';
+import { envConfig } from './common/config/app.config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      load: [envConfig],
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '../public/'),
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGO_DB),
     PokemonModule,
     CommonModule,
     SeedModule,
